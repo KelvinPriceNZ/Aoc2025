78286b1036386485562d4dea4dd666c0fcb42d4d
Kelvin <anonymous@nowhere.geek.nz>
2025-12-03 21:01:40 +1300

    remove print statements, change input file path

--- src/day01/part1.rkt
+++ src/day01/part1.rkt
@@ -12,7 +12,6 @@
         (unless (eof-object? line)
           (define direction (string-ref line 0))
           (define amount (string->number (substring line 1)))
-          (printf "~a Pos ~a Dir ~a Amount ~a" line position direction amount)
           (if (char=? direction #\R)
               (set! position (+ position amount))
               (set! position (- position amount))
@@ -21,11 +20,10 @@
               ( while (negative? position) (set! position (+ position 100)))
               ( void))
           (set! position (modulo position 100))
-          (printf " pos: ~a~n" position)
           (if (equal? position 0) (set! password (add1 password)) (void))
           (loop)))
       (printf "Password ~a~n" password)
       )))
 
 
-(process-file "../../input/01/input.txt")
+(process-file "../../input/day01/input.txt")
--- src/day01/part2.rkt
+++ src/day01/part2.rkt
@@ -12,7 +12,6 @@
         (unless (eof-object? line)
           (define direction (string-ref line 0))
           (define amount (string->number (substring line 1)))
-          (printf "~a Pos ~a Dir ~a Amount ~a Password ~a" line position direction amount password)
           (define inc 0)
           (if (char=? direction #\R)
               (set! inc 1)
@@ -25,10 +24,9 @@
               (when (zero? position)
                 (set! password (add1 password)))
               ))
-          (printf " pos: ~a   password: ~a~n" position password)
           (loop)))
       (printf "Password ~a~n" password)
       )))
 
 
-(process-file "../../input/01/input.txt")
+(process-file "../../input/day01/input.txt")
