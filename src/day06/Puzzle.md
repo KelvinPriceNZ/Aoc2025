4587ec57b88f4c87237ecdbe1f33dbd99ba5622e
Kelvin <anonymous@nowhere.geek.nz>
2025-12-13 09:34:09 +1300

    Day 6 part 1 solution

---
+++ src/day06/Puzzle.md
@@ -0,0 +1,5 @@
+# Advent of Code 2025
+
+## Day 6
+
+[2025 - Day 6](https://adventofcode.com/2025/day/6 "Advent of Code 2025 Day 6")
---
+++ src/day06/part1.rkt
@@ -0,0 +1,32 @@
+#lang racket
+
+(require algorithms)
+(require iso-printf)
+
+(define DAY_INPUT "../../input/day06/input.txt")
+
+(define lines (file->lines DAY_INPUT))
+
+(define calc '())
+
+(for ([line lines])
+  (set! calc (append calc (list (regexp-split #px"\\s+" line))))
+  )
+
+; transpose list of list
+(define ops (apply map list (reverse calc)))
+
+(define answer 0)
+
+(for ([e ops])
+  (define op (car e))
+  (define vals (map string->number (cdr e)))
+  (define result 0)
+  (cond
+    [(equal? op "*") (set! result (apply * vals))]
+    [(equal? op "+") (set! result (apply + vals))]
+    )
+  (set! answer (+ answer result))
+  )
+
+(printf "Answer: %d\n" answer)
