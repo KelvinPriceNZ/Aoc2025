5bf79d9a9491c2355141dcf65522a2282c15ee66
Kelvin <anonymous@nowhere.geek.nz>
2025-12-14 18:11:09 +1300

    Day 7 part 2 solution

---
+++ src/day07/part2.rkt
@@ -0,0 +1,46 @@
+#lang racket
+
+(require string-util)
+
+(define DAY_INPUT "../../input/day07/input.txt")
+
+(define lines (file->lines DAY_INPUT))
+
+(define rows (length lines))
+(define cols (string-length (list-ref lines 0)))
+
+(define (char-at grid row col)
+  (string (string-ref (list-ref grid row) col))
+  )
+
+
+(define beams (make-vector cols))
+(define answer 0)
+
+(vector-set! beams (string-first-char-occurrence (list-ref lines 0) #\S) 1)
+
+(for ([row (range 1 rows)])
+  (for ([col (range cols)])
+    (when (regexp-match? #px"[S|]" (char-at lines (sub1 row) col))
+      (when (equal? "." (char-at lines row col))
+        (string-set! (list-ref lines row) col #\|)
+        )
+      (when (equal? "^" (char-at lines row col))
+        (when (equal? "." (char-at lines row (sub1 col)))
+          (string-set! (list-ref lines row) (sub1 col) #\|)
+          )
+        (when (equal? "." (char-at lines row (add1 col)))
+          (string-set! (list-ref lines row) (add1 col) #\|)
+          )
+        (set! answer (add1 answer))
+        )
+      )
+    (when (equal? (string-ref (list-ref lines row) col) #\^)
+      (vector-set! beams (sub1 col) (+ (vector-ref beams (sub1 col)) (vector-ref beams col)))
+      (vector-set! beams (add1 col) (+ (vector-ref beams (add1 col)) (vector-ref beams col)))
+      (vector-set! beams col 0)
+    )
+  )
+)
+
+(printf "Answer: ~v~n" (apply + (vector->list beams)))
